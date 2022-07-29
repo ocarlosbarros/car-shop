83052c3569f7f6aa75b38500abdf6858f834129d
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-29 16:16:25 -0300

    refactor:changes export method

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,9 +1,7 @@
-interface IModel<T> {
+export interface IModel<T> {
   create(entity: T): Promise<T>,
   read(): Promise<Array<T>>,
   readOne(_id: string): Promise<T | null>,
   update(_id: string, entity: T): Promise<T | null>,
   delete(_id: string): Promise<T | null>,
 }
-
-export default IModel;
