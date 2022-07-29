1e0114eaef56c512b8bb71e490f7d7e55e0132c3
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-29 14:44:09 -0300

    feat:creates IModel interface

---
+++ src/interfaces/IModel.ts
@@ -0,0 +1,9 @@
+interface IModel<T> {
+  create(entity: T): Promise<T>,
+  read(): Promise<Array<T>>,
+  readOne(_id: string): Promise<T | null>,
+  update(_id: string, entity: T): Promise<T | null>,
+  delete(_id: string): Promise<T | null>,
+}
+
+export default IModel;
