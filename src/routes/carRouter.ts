31ea8909250645741dc1e57573211b769a6777c1
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-29 22:19:43 -0300

    feat:creates carRouter with post method

---
+++ src/routes/carRouter.ts
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import CarController from '../controllers/CarController';
+import CarService from '../services/CarService';
+import Car from '../models/Car';
+
+const carModel = new Car();
+const carService = new CarService(carModel);
+const carController = new CarController(carService);
+
+const carRouter = Router();
+
+carRouter.post('/', carController.create);
+
+export default carRouter;
