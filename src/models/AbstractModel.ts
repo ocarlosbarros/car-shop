13fa0c5bde9716cf9fec340bb078e1c435ec40b5
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-29 22:17:26 -0300

    feat:creates AbstractModel

---
+++ src/models/AbstractModel.ts
@@ -0,0 +1,31 @@
+import { Model } from 'mongoose';
+import { IModel } from '../interfaces/IModel';
+
+abstract class AbstractModel<T> implements IModel<T> {
+  protected _model: Model<T>;
+  
+  constructor(model: Model<T>) {
+    this._model = model;
+  }
+  
+  public async create(entity: T): Promise<T> {
+    const created = await this._model.create({ ...entity });
+    return created;
+  }
+
+  //   read(): Promise<T[]> {
+  //     throw new Error('Method not implemented.');
+  //   }
+
+//   readOne(_id: string): Promise<T | null> {
+//     throw new Error('Method not implemented.');
+//   }
+//   update(_id: string, entity: T): Promise<T | null> {
+//     throw new Error('Method not implemented.');
+//   }
+//   delete(_id: string): Promise<T | null> {
+//     throw new Error('Method not implemented.');
+//   }
+}
+
+export default AbstractModel;
