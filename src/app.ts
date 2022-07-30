0b1d9cb1b8bc55af0e1bf50a9dcf66f9d8980966
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-29 22:22:04 -0300

    feat:implements middleware for application use

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import router from './routes/index';
+import errorHandler from './middleware/errorHandler';
 
 const app = express();
+
+app.use(express.json());
+app.use(router);
+app.use(errorHandler);
 
 export default app;
