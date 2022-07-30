a8e044e8535f3441827d7fc2110755cba578db33
Carlos Barros <carlos_dbs@hotmail.com>
2022-07-29 22:18:09 -0300

    feat:creates CarService and implements create method

---
+++ src/services/CarService.ts
@@ -0,0 +1,23 @@
+import IService from '../interfaces/IService';
+import carZodSchema, { ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+
+class CarService implements IService<ICar> {
+  private _car: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._car = model;
+  }
+
+  public async create(entity: ICar): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(entity);
+
+    if (!parsed.success) {
+      throw parsed.error;
+    }
+    const created = await this._car.create(entity);
+    return created;
+  }
+}
+
+export default CarService;
