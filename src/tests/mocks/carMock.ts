dbdfd0b36aee80cb6427ad83cc59dcc4a96dcea1
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-03 22:27:26 -0300

    refactor(carMock): adds carMockWithId removing id of carMock

--- src/tests/mocks/carMock.ts
+++ src/tests/mocks/carMock.ts
@@ -1,6 +1,15 @@
 import { ICar } from '../../interfaces/ICar';
 
-const carMock: ICar & { _id: string } = {
+const carMock: ICar = {
+    model: 'Chevrolet Impala',
+    year: 1967,
+    color: 'Black',
+    buyValue: 50000,
+    seatsQty: 6,
+    doorsQty: 4,
+}
+
+const carMockWithId: ICar & { _id: string } = {
     _id: '507f1f77bcf86cd799439011',
     model: 'Chevrolet Impala',
     year: 1967,
@@ -10,4 +19,4 @@ const carMock: ICar & { _id: string } = {
     doorsQty: 4,
 }
 
-export default carMock;
+export { carMock, carMockWithId };
