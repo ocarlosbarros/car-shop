8b97a3f05d7a41ba0d09094e9b2ab08cc36ef2b5
Carlos Barros <carlos_dbs@hotmail.com>
2022-08-07 14:30:11 -0300

    refactor(IModel):uncomment the read method

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -1,6 +1,6 @@
 export interface IModel<T> {
   create(entity: T): Promise<T>,
-  // read(): Promise<Array<T>>,
+  read(): Promise<Array<T>>,
   // readOne(_id: string): Promise<T | null>,
   // update(_id: string, entity: T): Promise<T | null>,
   // delete(_id: string): Promise<T | null>,
